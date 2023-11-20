2f9150a20558c0bd1a4027637e6149b210290f0f
João Vítor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2023-11-20 17:13:03 -0300

    add mulitiplicação e divisão

--- calculator.py
+++ calculator.py
@@ -3,5 +3,11 @@ class Calculator:
   def sum(self, a, b):
     return a + b
   
-  def subtraction(self, a, b):
+  def subtract(self, a, b):
     return a - b
+  
+  def mult(self, a, b):
+    return a * b
+  
+  def div(self, a, b):
+    return a / b
--- test_calculator.py
+++ test_calculator.py
@@ -12,7 +12,17 @@ class TestCalculator:
         assert(self.calc.sum(1, 2) == 3)
 
     def test_subtraction(self):
-        assert(self.calc.subtraction(5, 4) == 1)
+        assert(self.calc.subtract(5, 4) == 1)
 
     def test_subtraction_negative_result(self):
-        assert(self.calc.subtraction(6, 7) == -1)
+        assert(self.calc.subtract(6, 7) == -1)
+
+    def test_multiplication(self):
+        assert(self.calc.mult(3, 2) == 6)
+
+    def test_division(self):
+        assert(self.calc.div(10, 5) == 2)
+
+    def test_division_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+          self.calc.div(10,0)
